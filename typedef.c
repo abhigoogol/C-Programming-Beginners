f5acb1c18e1645baffcd15cd65136c3d58d8a6ee
Abhishek <abhishek.sambyal@gmail.com>
2015-12-22 12:22:36 +0530

    Adding struct in typedef

--- typedef.c
+++ typedef.c
@@ -2,12 +2,21 @@
 
 typedef char MYSTRING;
 
+typedef struct node {
+    //int * lp;
+    int data;
+    //int * rp;
+} MYNODE;
+
 int main()
 {
     int i = 10;
     MYSTRING mystring[20] = "Hello";
+    MYNODE a;
+    a.data = 5;
 
     printf("%d\n", i);
-    printf("%s\n", mystring);
+    printf("mystring = %s\n", mystring);
+    printf("a.data = %d\n", a.data);
     return 0;
 }
